8c556d2b8559dfdd24739476f1c87e2ae9832be4
tstieven <thomas.stievenard@orange.fr>
2018-01-25 23:53:59 -0500

    first commit

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class CameraFollow : MonoBehaviour
+{
+    public float smoothing = 5f;        // The speed with which the camera will be following.
+
+    int nbPlayer;
+    GameObject[] listPlayer;
+
+    Vector3 offset;                     // The initial offset from the target.
+
+    void Start()
+    {
+        // Get the list of player
+        listPlayer = GameObject.FindGameObjectsWithTag("Player");
+        Vector3 target = new Vector3(0,0,0);
+        foreach (GameObject player in listPlayer)
+        {
+            target += player.transform.position;
+        }
+        nbPlayer = listPlayer.Length;
+        
+
+        // Calculate the initial offset.
+        offset = transform.position - target / nbPlayer;
+
+    }
+
+    void FixedUpdate()
+    {
+       
+        // Create a postion the camera is aiming for based on the offset from the target.
+        Vector3 targetCamPos= new Vector3(0,0,0);
+         Vector3 lastPlayer = targetCamPos;
+         Vector3 newPlayer;
+         int notYet = 0;
+         foreach (GameObject player in listPlayer)
+         {
+             newPlayer = player.transform.position;
+            targetCamPos += newPlayer;
+             if(targetCamPos == newPlayer)
+             {
+                 lastPlayer = newPlayer;
+             }
+             else
+             {
+                 if (Mathf.Max(Mathf.Abs(lastPlayer.x- newPlayer.x),Mathf.Abs(lastPlayer.z- newPlayer.z)) > 15)
+                 {
+                     notYet = 1;
+
+                 }
+                 else { lastPlayer = newPlayer; }
+             }
+         }
+       
+        if (notYet != 1)
+        {
+            targetCamPos = targetCamPos / nbPlayer + offset;
+
+            // Smoothly interpolate between the camera's current position and it's target position.
+            transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+        }
+    }
+       
+}
