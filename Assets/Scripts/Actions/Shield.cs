d0e0e2c48034b0b8fc8d970cef8c449fa00d15c1
florian.desrousseaux <florian.desrousseaux@isen-lille.fr>
2018-02-04 10:01:37 -0500

    blockage bouclier alpha

---
+++ Assets/Scripts/Actions/Shield.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield : MonoBehaviour {
+
+    public bool isCountering;
+    private float shieldTime;
+    private float hitTime;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    void Awake ()
+    {
+        isCountering = false;
+        shieldTime = Time.time;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Weapons" && col.GetComponentInParent<Attack>().isAttacking)
+        {
+            isCountering = true;
+            hitTime = Time.time;
+            if(hitTime - shieldTime < 0.5)
+            {
+                Debug.Log("perfect counter");
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        isCountering = false;
+    }
+}
--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -5,21 +5,30 @@ using UnityEngine;
 public class PlayerHealth : MonoBehaviour {
 
     Rigidbody playerRigidbody;
+    Shield shield;
     Vector3 knockback;
 
     void Awake()
     {
         playerRigidbody = GetComponent<Rigidbody>();
+        shield = GetComponentInChildren<Shield>();
     }
 
     void OnTriggerEnter(Collider col)
     {
         if (col.gameObject.tag == "Weapons" && col.GetComponentInParent<Attack>().isAttacking)
         {
-            Debug.Log("You hurt me!!!");
-            knockback = col.transform.forward;
-            KnockBack(knockback);
-        }
+            if (shield.isCountering)
+            {
+                Debug.Log("I blocked an attack !");
+            }
+            else
+            {
+                Debug.Log("You hurt me!!!");
+                knockback = col.transform.forward;
+                KnockBack(knockback);
+            }
+        } 
     }
 
     void KnockBack(Vector3 k)
