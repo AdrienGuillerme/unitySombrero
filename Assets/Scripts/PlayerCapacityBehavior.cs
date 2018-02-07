a4342e614fef25c2e4eb37769cd0f6025d24ba85
fduport <francois.duport@hotmail.fr>
2018-02-06 21:17:17 -0500

    add antoine part

---
+++ Assets/Scripts/PlayerCapacityBehavior.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCapacityBehavior : MonoBehaviour {
+
+    public string controllerName = "Joy1";
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    void FixedUpdate()
+    {
+        // Store the input button.
+        bool capacityIsTriggered = false;
+
+        if (controllerName == "Keyboard")
+        {
+            
+        }
+        else
+        {
+            capacityIsTriggered = Input.GetButtonDown(controllerName + "Capacity");
+
+            if (capacityIsTriggered)
+            {
+                // choose the capacity to trigger
+                Debug.Log("Fiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiire");
+                GameObject obj = Resources.Load("Glyph") as GameObject;
+                Vector3 position = new Vector3(transform.position.x, transform.position.y, transform.position.z); 
+                Instantiate(obj, position, transform.rotation);
+            }
+        }
+
+        // Animate the player.
+        Animating(capacityIsTriggered);
+    }
+
+    void Animating(bool capacityIsTriggered)
+    {
+       
+    }
+}
---
+++ Assets/Scripts/SpecialCapacityGlyph.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpecialCapacityGlyph : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            // TODO : ajouter nom du script à appeler + fonction make damage
+            // other.gameObject.GetComponentInChildren<>
+            Debug.Log("Glyph activated");
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
