3a30552db112268df3e7fd78c00d7e619fd0b414
tstieven <thomas.stievenard@orange.fr>
2018-02-14 20:50:26 -0500

    add score

--- Assets/Scripts/LoadUI.cs
+++ Assets/Scripts/LoadUI.cs
@@ -11,43 +11,42 @@ public class LoadUI : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-        
+        GameObject[] users = GameObject.FindGameObjectsWithTag("User");
         GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
         //Player Tag only for CharacterGroup->Player
-        int cpt = 1;
-        foreach(GameObject player in players)
+        int cpt = 0;
+        foreach (GameObject player in players)
         { GameObject newHealthUI;
-            if (cpt%2 == 0)
+            if (cpt % 2 == 1)
             {
-                newHealthUI = Instantiate(heathUIRight,hudCanvas.transform);
-                if (cpt == 4)
+                newHealthUI = Instantiate(heathUIRight, hudCanvas.transform);
+                if (cpt == 3)
                 {
                     newHealthUI.GetComponent<RectTransform>().SetInsetAndSizeFromParentEdge(RectTransform.Edge.Bottom, 60, 1);
-                } 
+                }
             }
             else
             {
 
-                newHealthUI =Instantiate(heathUILeft,hudCanvas.transform);
-                if (cpt == 3)
+                newHealthUI = Instantiate(heathUILeft, hudCanvas.transform);
+                if (cpt == 2)
                 {
-                    
-                        newHealthUI.GetComponent<RectTransform>().SetInsetAndSizeFromParentEdge(RectTransform.Edge.Bottom, 60, 1);
-                    
+
+                    newHealthUI.GetComponent<RectTransform>().SetInsetAndSizeFromParentEdge(RectTransform.Edge.Bottom, 60, 1);
+
                 }
             }
 
             PlayerHealth newPlayerHealth = player.GetComponent<PlayerHealth>();
             //Debug.Log(newHealthUI.GetComponentInChildren<Slider>());
             newPlayerHealth.healthSlider = newHealthUI.GetComponentInChildren<Slider>();
+            GameObject user = users[cpt];
+            DontDestroy newUserScore = user.GetComponent<DontDestroy>();
+            newUserScore.uiText = newHealthUI.GetComponentInChildren <Text>();
+            newUserScore.AddScore(0);
             cpt++;
         }
 
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
 		
 	}
 }
