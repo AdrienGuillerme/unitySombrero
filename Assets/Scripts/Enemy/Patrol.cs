097c06c3c754594e38de859ef925675f3810fc86
AdrienGuillerme <adrien.guillerme@isen.yncrea.fr>
2018-02-18 10:39:40 -0500

    new enemy structure

---
+++ Assets/Scripts/Enemy/Patrol.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : StateMachineBehaviour {
+
+    public float patrolSpeed = 3f;
+    public float patrolDistance = 15f;
+
+    private Rigidbody enemyRigidbody;
+    private Transform enemyTransform;
+
+    private Vector3 movement;
+    private float distanceTraveled = 0f;
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        enemyRigidbody = animator.gameObject.GetComponent<Rigidbody>();
+        movement = enemyRigidbody.transform.forward;
+    }
+
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (distanceTraveled < patrolDistance)
+        {
+            Move();
+        }
+        else
+        {
+            Turn();
+            distanceTraveled = 0f;
+        }
+    }
+
+    void Move()
+    {
+        movement = movement.normalized * patrolSpeed * Time.deltaTime;
+        distanceTraveled += Vector3.Distance(new Vector3(0, 0, 0), movement);
+        enemyRigidbody.MovePosition(enemyRigidbody.transform.position + movement);
+    }
+
+    void Turn()
+    {
+        enemyRigidbody.transform.forward = -enemyRigidbody.transform.forward;
+        movement = enemyRigidbody.transform.forward;
+    }
+}
---
+++ Assets/Scripts/Enemy/PlayerDetection.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDetection : MonoBehaviour {
+
+    private Collider playerDetector;
+    private Animator animator;
+
+    void Start () {
+        playerDetector = GetComponent<CapsuleCollider>();
+        animator = GetComponentInParent<Animator>();
+    }
+
+    void OnTriggerEnter (Collider col) {
+        AnimatorStateInfo state = animator.GetCurrentAnimatorStateInfo(0);
+        if ((state.IsName("Patrol") || state.IsName("idle")) && col.gameObject.tag == "Player")
+        {
+            animator.SetTrigger("Pursuit");
+        }
+        //if (state.IsName("Pursuit") && target)
+        //{
+        //    animator.SetBool("IsPursuing", false);
+        //}
+    }
+}
