b632321d0ee5f31a95fa82d7b2b9a42e4da6ab1a
fduport <francois.duport@hotmail.fr>
2018-02-19 13:00:00 -0500

    add rez

--- Assets/Scripts/Player/Resurection.cs
+++ Assets/Scripts/Player/Resurection.cs
@@ -11,22 +11,21 @@ public class Resurection : MonoBehaviour
     //public AudioClip deathClip;                                 // The audio clip to play when the player dies.
     //Animator anim;                                              // Reference to the Animator component.
     //AudioSource playerAudio;                                    // Reference to the AudioSource component.
-
-    PlayerHealth tmpHealth; // Reference to an other character health
+    Transform trans;                                            //transform charactergroup
+    Resurection resScript; // Reference to an other character rez script
     PlayerHealth myHealth;  // Reference to my own health function
-    PlayerHealth characterHealth; // Reference to a health function to keep
+    public PlayerHealth characterHealth; // Reference to a health function to keep
 
     public double revSpeed;                                     //Divise le nombre de point a atteindre par le nombre rentré
 
-    private bool canRevive;
-    private Collider col;
 
     private void Start()
     {
         //init gameController
+        trans = this.GetComponentInParent<Transform>().parent.transform;
         DontDestroy parentFunction = GetComponentInParent<DontDestroy>();
         controllerName = parentFunction.controllerName;
-        myHealth = GetComponentInParent<PlayerHealth>();
+        myHealth =trans.Find("Player").GetComponent<PlayerHealth>();
 
     }
 
@@ -36,21 +35,13 @@ public class Resurection : MonoBehaviour
         // anim = GetComponent<Animator>();
         //playerAudio = GetComponent<AudioSource>();
     }
-
+    /**/
     void OnTriggerStay(Collider col)
     {
-        if ((col.gameObject.tag == "Player") && (!myHealth.IsDead()))
+        if(col.gameObject.tag == "Player")
         {
-            tmpHealth = col.GetComponent<PlayerHealth>();
-            if ((tmpHealth.IsDead()) && (!tmpHealth.IsRevived()))
-            {
-                canRevive = true;
-                characterHealth = tmpHealth;
-            }
-            else
-            {
-                canRevive = false;
-            }
+            resScript = col.gameObject.GetComponentInParent<Transform>().parent.GetComponentInParent<Transform>().Find("ReviveCollider").GetComponentInChildren<Resurection>();
+            resScript.SetHealth(myHealth);
         }
     }
 
@@ -58,19 +49,29 @@ public class Resurection : MonoBehaviour
     {
         if (col.gameObject.tag == "Player")
         {
-            tmpHealth = col.GetComponent<PlayerHealth>();
-            if (tmpHealth == characterHealth)
-            {
-                canRevive = false;
-                characterHealth = null;
-            }
+            resScript = col.gameObject.GetComponentInParent<Transform>().parent.GetComponentInParent<Transform>().Find("ReviveCollider").GetComponentInChildren<Resurection>();
+            resScript.RemoveHealth(myHealth);
         }
     }
 
 
     void Update()
     {
-        if (canRevive)
+        if (characterHealth != null && !characterHealth.IsDead())
+            this.RemoveHealth(characterHealth);
+
+        if(!canRevive())
+        {
+            BoxCollider box = this.GetComponent<BoxCollider>();
+            box.enabled = true;
+        }
+        else
+        {
+            BoxCollider box = this.GetComponent<BoxCollider>();
+            box.enabled = false;
+        }
+
+        if (canRevive() && characterHealth != null && characterHealth.IsDead())
         {
             if (Input.GetButton(controllerName + "Action"))
             {
@@ -79,11 +80,28 @@ public class Resurection : MonoBehaviour
         }
     }
 
+    private bool canRevive()
+    {
+        return (!myHealth.IsDead());
+    }
+
     void Revive()
     {
         characterHealth.getRevive();
         Debug.Log("I'm healing!");
 
+    }
+
+    void SetHealth(PlayerHealth health)
+    {
+        if (characterHealth == null)
+            characterHealth = health;
+    }
+
+    void RemoveHealth(PlayerHealth health)
+    {
+        if (characterHealth == health)
+            characterHealth = null;
     }
 }
 
