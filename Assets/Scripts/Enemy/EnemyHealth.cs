17735f4ff2da09150edea6f0915fa9573c3781b8
mrohmer <martin.rohmer@polytech-lille.net>
2018-02-21 17:50:54 -0500

    global improvements

--- Assets/Scripts/Enemy/EnemyHealth.cs
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -7,7 +7,7 @@ public class EnemyHealth : MonoBehaviour {
     public int maxHealth = 3;
 
     private int health;
-    private bool isDead;
+    public bool isDead;
 
     Rigidbody enemyRigidbody;
     Animator animator;
@@ -24,13 +24,13 @@ public class EnemyHealth : MonoBehaviour {
     {
         if (col.gameObject.tag == "Weapons")
         {
-            getHurt(1);
+            GetHurt(1);
             knockback = col.transform.forward;
             KnockBack(knockback);
         }
     }
 
-    public void getHurt(int i)
+    public void GetHurt(int i)
     {
         health -= i;
         if (health <= 0 && !isDead)
