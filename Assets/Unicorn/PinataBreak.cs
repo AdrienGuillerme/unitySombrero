6156f776bea3495dfb4c0a9b6794a0f28f789dc0
florian.desrousseaux <florian.desrousseaux@isen-lille.fr>
2018-03-11 20:08:15 -0400

    some lighting change + asset compression + pinata

---
+++ Assets/Unicorn/PinataBreak.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PinataBreak : MonoBehaviour {
+
+    private Transform parent;
+    private GameObject pinata_normal;
+    private GameObject pinata_broken;
+    public bool breakPinata;
+    private bool broken = false;
+    public int maxhp = 100;
+    public int minhp = 10;
+    public int hp;
+
+	void Start () {
+        parent = transform.parent;
+        pinata_broken = parent.Find("Unicorn_Shatters").gameObject;
+        pinata_normal = parent.Find("Unicorn").gameObject;
+        hp = Random.Range(minhp, maxhp);
+    }
+	
+	void Update () {
+        if (breakPinata && !broken)
+        {
+            BreakPinata();
+        }
+        if(hp <= 0 && !broken)
+        {
+            Die();
+        }
+    }
+
+    public void BreakPinata()
+    {
+        pinata_normal.SetActive(false);
+        pinata_broken.SetActive(true);
+        broken = true;
+    }
+
+    private void Die()
+    {
+        Debug.Log("i'm dead");
+        BreakPinata();
+        this.enabled = false;
+    }
+
+    private void LoseHP(int value)
+    {
+        this.hp -= value;
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        Debug.Log("beep");
+        if (col.gameObject.CompareTag("Weapons"))
+        {
+            LoseHP(10);
+        }
+    }
+}
