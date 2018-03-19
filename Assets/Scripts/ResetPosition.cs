b8b712740adca05bf69adb82628186760178c731
tstieven <thomas.stievenard@orange.fr>
2018-03-19 14:33:26 -0400

    add scene Credits

---
+++ Assets/Scripts/ResetPosition.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetPosition : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GameObject[] users = GameObject.FindGameObjectsWithTag("CharacterGroup");
+        int cpt = 0;
+        foreach (GameObject user in users)
+        {
+            user.GetComponent<Transform>().SetPositionAndRotation(new Vector3(cpt, (float)0.5, cpt), new Quaternion(0, 0, 0, 0));
+        }
+    }
+	
+}
