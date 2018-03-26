a6c755849905106df8947a58b6ea125883ee58ff
tstieven <thomas.stievenard@orange.fr>
2018-03-26 13:44:50 -0400

    add ResetLife

--- Assets/Scripts/ResetPosition.cs
+++ Assets/Scripts/ResetPosition.cs
@@ -11,6 +11,7 @@ public class ResetPosition : MonoBehaviour {
         foreach (GameObject user in users)
         {
             user.GetComponent<Transform>().SetPositionAndRotation(new Vector3(cpt, (float)0.5, cpt), new Quaternion(0, 0, 0, 0));
+            user.GetComponentInChildren<PlayerHealth>().ResetLife();
         }
     }
 	
