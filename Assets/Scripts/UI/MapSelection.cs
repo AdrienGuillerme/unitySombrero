1a57e613e255b0f110473adbacdce4c90c664045
AdrienGuillerme <adrien.guillerme@isen.yncrea.fr>
2018-04-10 09:55:40 -0400

    fix map selection

--- Assets/Scripts/UI/MapSelection.cs
+++ Assets/Scripts/UI/MapSelection.cs
@@ -8,12 +8,13 @@ public class MapSelection : MonoBehaviour {
 
     public Image map1;
     public string name1;
+    public string scene1;
     public Image map2;
     public string name2;
-    public Image map3;
-    public string name3;
+    public string scene2;
     List<Image> listImage = new List<Image>();
     List<string> listName = new List<string>();
+    List<string> listSceneName = new List<string>();
 
     public GameObject objText;   
     public GameObject objMap;
@@ -36,13 +37,14 @@ public class MapSelection : MonoBehaviour {
             controllers.Add(user.GetComponent<DontDestroy>().controllerName);
         }
 
-        //listImage.Add(map1);
+        listImage.Add(map1);
         listImage.Add(map2);
-        listImage.Add(map3);
 
-        //listName.Add(name1);
+        listName.Add(name1);
         listName.Add(name2);
-        listName.Add(name3);
+
+        listSceneName.Add(scene1);
+        listSceneName.Add(scene2);
 
         nbMap = listImage.Count;
         map = objMap.GetComponent<Image>();
@@ -90,14 +92,14 @@ public class MapSelection : MonoBehaviour {
                     {
                         user.gameObject.SetActive(true);
                     }*/
-                    if (cpt == 0)
+                    if (cpt == nbMap - 1)
                     {
                         foreach (GameObject user in users)
                         {
                             DestroyObject(user.gameObject);
                         }
                     }
-                    SceneManager.LoadScene(listName[cpt]);
+                    SceneManager.LoadScene(listSceneName[cpt]);
                 }
             }
            
